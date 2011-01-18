a75f706b9b0f58d24874036f4258f6e5b7d54445
Ameer Deen <writeameer@gmail.com>
2011-01-18 12:45:31 +1100

    Added Timezone recipe for Windows

---
+++ cookbooks/utilities/recipes/sys_timezone_set_windows.rb
@@ -0,0 +1,32 @@
+# Cookbook Name:: utilities
+# Recipe:: sys_timezone_set_windows
+#
+# Copyright 2010, RightScale, Inc.
+#
+# All rights reserved
+
+powershell "Sets Windows Timezone" do
+  attachments_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'files', 'sys_timezone_set_windows'))
+  parameters({'ATTACHMENTS_PATH' => attachments_path})
+
+  # Create the powershell script
+  powershell_script = <<'POWERSHELL_SCRIPT'
+  
+    # Set Error action preference
+	$errorActionPreference = "stop"
+	
+	# Get Timezone from input variable
+	#$tzset = @node[:utilities][:time_zone]
+	
+	
+	#
+	# Set the Timezone
+	#
+
+	cd "$env:ATTACHMENTS_PATH"
+	Start-Process -FilePath ".\TimezoneTool.exe" -RedirectStandardError "error.txt" -RedirectStandardOutput "output.txt" -ArgumentList '"Pacific Standard Time"'
+
+POWERSHELL_SCRIPT
+
+  source(powershell_script)
+end
