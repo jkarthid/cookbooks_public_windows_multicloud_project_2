39a2cc2d80a98c31b04f8d12f38736773b1c1aa5
Ameer Deen <writeameer@gmail.com>
2011-01-18 12:32:12 +1100

    Added TimeZone Recipe for Windows

--- cookbooks/utilities/metadata.rb
+++ cookbooks/utilities/metadata.rb
@@ -14,6 +14,8 @@ recipe "utilities::install_7zip", "Installs 7-Zip"
 recipe "utilities::create_scheduled_task", "Creates the 'rs_scheduled_task' scheduled task under the 'administrator' user. Uses the SCHTASKS Windows command"
 recipe "utilities::delete_scheduled_task", "Deletes the 'rs_scheduled_task' scheduled task under the 'administrator' user. Uses the SCHTASKS Windows command"
 recipe "utilities::online_attached_drives", "Change 'Offline' status to 'Online' for the attached drives"
+recipe "utilities::sys_timezone_set_windows", "Set timezone on Windows Servers"
+
 
 attribute "utilities/admin_password",
   :display_name => "New administrator password",
