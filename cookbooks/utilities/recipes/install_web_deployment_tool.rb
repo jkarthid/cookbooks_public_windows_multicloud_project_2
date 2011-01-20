f35290bab1e2263be43eaa9a89e7f3da033469e4
Ameer Deen <writeameer@gmail.com>
2011-01-20 16:45:05 +1100

    Added installer for Microsoft Web Deployment Tool

---
+++ cookbooks/utilities/recipes/install_web_deployment_tool.rb
@@ -0,0 +1,44 @@
+# Cookbook Name:: utilities
+# Recipe:: sys_timezone_set_windows
+#
+# Copyright 2010, RightScale, Inc.
+#
+# All rights reserved
+
+
+powershell "Installs Web Deployment Tool" do
+  attachments_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'files', 'sys_timezone_set_windows'))
+  parameters({'ATTACHMENTS_PATH' => attachments_path,'TIMEZONE' => @node[:utilities][:timezone]})
+
+  
+  # Create the powershell script
+  powershell_script = <<'POWERSHELL_SCRIPT'
+    # Execute only on first boot
+    if ($env:RS_REBOOT -eq $true) { exit 0 } 
+	
+    # Set Error action preference
+	$errorActionPreference = "stop"
+	
+
+	# Set the Timezone using input variable
+	switch ($env:TIMEZONE) 
+	{
+		"US/Central" {$tzset="Central Standard Time"}
+		"GMT" {$tzset="GMT Standard Time"}
+		"UTC" {$tzset="UTC"}
+		"Europe/Helsinki" {$tzset="FLE Standard Time"}
+		"Europe/Moscow" {$tzset="Russian Standard Time"}
+		"US/Mountain" {$tzset="Mountain Standard Time"}
+		"US/Pacific" {$tzset="Pacific Standard Time"}
+		"US/Eastern" {$tzset="Eastern Standard Time"}
+		"Europe/London" {$tzset="GMT Standard Time"}
+		"Europe/Paris" {$tzset="Romance Standard Time"}
+		default { $tzset = "UTC" } 
+	}
+	
+	cd "$env:ATTACHMENTS_PATH"
+	Start-Process -FilePath ".\TimezoneTool.exe" -RedirectStandardError "error.txt" -ArgumentList """$tzset"""
+POWERSHELL_SCRIPT
+
+  source(powershell_script)
+end
